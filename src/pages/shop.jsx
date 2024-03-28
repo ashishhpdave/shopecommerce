1d7e3a07be941c1a6f374059a184bbd2f9704455
ashishhpdave <141114347+ashishhpdave@users.noreply.github.com>
2024-03-28 15:18:02 +0530

    OfferBanner

--- src/pages/shop.jsx
+++ src/pages/shop.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../Commponents/Assets/Hero/Hero";
 import Popular from "../Commponents/Assets/Popular/Popular";
+import Offers from "../Commponents/Offers/Offers";
 
 const Shop = () =>{
     return(
         <div>
             <Hero />
             <Popular />
+            <Offers />
         </div>
     )
 }
