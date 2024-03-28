365bd46ff6547376e79d76a2829fa7e4eb9bf86e
ashishhpdave <141114347+ashishhpdave@users.noreply.github.com>
2024-03-28 15:01:01 +0530

    Hero&Product

--- src/pages/shop.jsx
+++ src/pages/shop.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Hero from "../Commponents/Assets/Hero/Hero";
+import Popular from "../Commponents/Assets/Popular/Popular";
 
 const Shop = () =>{
     return(
         <div>
-
+            <Hero />
+            <Popular />
         </div>
     )
 }
