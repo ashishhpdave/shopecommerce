42df28b017600e06ec77fc2c4b506092b3329cfe
ashishhpdave <141114347+ashishhpdave@users.noreply.github.com>
2024-03-30 14:03:17 +0530

    CartItems&Checkout&promocode

--- src/Commponents/Assets/Navbar/Navbar.jsx
+++ src/Commponents/Assets/Navbar/Navbar.jsx
@@ -1,12 +1,13 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import './Navbar.css'
 import logo from '../logo.png'
 import cart_icon from '../cart_icon.png'
 import { NavLink } from "react-router-dom";
+import { ShopContext } from "../../../Context/ShopContext";
 const Navbar = () =>{
 
     const [menu,setMenu] = useState("shop")
-
+    const {getDefultCartItems} = useContext(ShopContext);
     return(
         <div className="navbar">
             <div className="nav-logo">
@@ -22,7 +23,7 @@ const Navbar = () =>{
             <div className="nav-login-cart">
                <NavLink to='/login'> <button>Login</button>  </NavLink>
                <NavLink to='/cart'><img src={cart_icon} alt="" /></NavLink> 
-                <div className="nav-cart-count">0</div>
+                <div className="nav-cart-count">{getDefultCartItems()}</div>
             </div>
         </div>
     )
--- src/Commponents/Cartitems/Cartitems.jsx
+++ src/Commponents/Cartitems/Cartitems.jsx
@@ -3,7 +3,7 @@ import './Cartitems.css'
 import { ShopContext } from "../../Context/ShopContext";
 import remove_icon from '../Assets/cart_cross_icon.png'
 const CartItems = () =>{
-    const {all_product,cartItems,removeFromCart} = useContext(ShopContext);
+    const {getTotalCartAmount,all_product,cartItems,removeFromCart} = useContext(ShopContext);
     return(
         <div className="cartitem">
             <div className="cartitems-format-main">
@@ -38,7 +38,7 @@ const CartItems = () =>{
                     <div>
                         <div className="cartitems-total-item">
                             <p>Subtotal</p>
-                            <p>${0}</p>
+                            <p>${getTotalCartAmount()}</p>
                         </div>
                         <hr />
                         <div className="cartitems-total-item">
@@ -48,7 +48,7 @@ const CartItems = () =>{
                         <hr />
                         <div className="cartitems-total-item">
                             <h3>Total</h3>
-                            <h3>${0}</h3>
+                            <h3>${getTotalCartAmount()}</h3>
                         </div>
                     </div>
                     <button>PROCEED TO CHECKOUT</button>
